f594b7eaa773b73cfef2706dc64fde5380232459
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-11-02 13:42:54 +0000

    Handle player's move and update board accordingly

--- src/main/resources/public/game.js
+++ src/main/resources/public/game.js
@@ -1,5 +1,6 @@
 $(function() {
 	'use strict';
+	var MOVE_URL_ = 'player-move';
 	var NEW_GAME_URL_ = 'new-game';
 
 	function Game() {
@@ -17,6 +18,21 @@ $(function() {
 	Game.prototype.createBoard = function() {
 		this.board_ = $(this.boardTemplate_);
 		this.outlet_.html(this.board_);
+		this.board_.on('click', 'td', this.handleGameMove.bind(this));
 	};
+
+	Game.prototype.updateCell = function(data) {
+		this.state_ = JSON.parse(data);
+		this.currentCell_.text(this.state_.player);
+	};
+
+	Game.prototype.handleGameMove =	function(event) {
+		var cell = $(event.target);
+		this.currentCell_ = cell;
+
+		$.post(MOVE_URL_, {move: this.currentCell_.data('col')})
+			.done(this.updateCell.bind(this));
+	};
+
 	new Game();
 });
