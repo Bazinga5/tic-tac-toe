6845d6a7e0c244bbf6c9d7c12ac0220da195e8ba
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-11-02 13:34:48 +0000

    Server should implement a spark applications

--- src/main/java/is/ru/bazinga/Web.java
+++ src/main/java/is/ru/bazinga/Web.java
@@ -1,11 +1,22 @@
 package is.ru.bazinga;
 
-import spark.*;
+import spark.servlet.SparkApplication;
+
 import static spark.Spark.*;
 
-public class Web {
+public class Web implements SparkApplication {
+  Tic game;
+
   public static void main(String[] args) {
     staticFileLocation("/public");
-    get("/hello", (request, response) -> "Hello World!");
+    SparkApplication web = new Web();
+    web.init();
+  }
+
+  @Override
+  public void init() {
+    post("/new-game", (req, res) -> {
+      game = new Tic();
+      return true;
   }
 }
